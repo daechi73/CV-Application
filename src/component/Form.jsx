e794ff188d73d3783b7d08c8ef364be048cca0a1
J <59581793+daechi73@users.noreply.github.com>
2023-08-04 19:11:01 -0400

    form where user inputs their information, setValue state is used to set value to the value state in app.js

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,56 +1,116 @@
 import "./Form.css";
+import { useState } from "react";
 
-const Form = () => {
+const Form = (props) => {
+  const changeValue = (e) => {
+    let temp = e.target.id.split("-").splice(1);
+    temp = "CV-" + temp[0] + "-" + temp[1];
+    const value = {
+      ...props.inputValue,
+      [`${temp}`]: e.target.value,
+    };
+    console.log(temp);
+    console.log(value);
+    props.setValue(value);
+  };
   return (
     <>
       <form className="forms">
         <section className="form-personalDetail form">
-          <div className="personalDetail-title">Personal Details:</div>
-          <label htmlFor="personalDetail-name">Full Name:</label>
-          <input type="text" id="personalDetail-name" />
-          <label htmlFor="personalDetail-email">Email:</label>
-          <input type="text" id="personanlDetail-email" />
-          <label htmlFor="personalDetail-phone">Phone number:</label>
-          <input type="number" id="personalDetail-phone" />
-          <label htmlFor="personanlDetail-address">Address:</label>
-          <input type="text" id="personalDetail-address" />
+          <div className="form-personalDetail-title">Personal Details:</div>
+          <label htmlFor="form-personalDetail-name">Full Name:</label>
+          <input
+            onChange={changeValue}
+            type="text"
+            id="form-personalDetail-name"
+          />
+          <label htmlFor="form-personalDetail-email">Email:</label>
+          <input
+            onChange={changeValue}
+            type="email"
+            id="form-personalDetail-email"
+          />
+          <label htmlFor="form-personalDetail-phone">Phone number:</label>
+          <input
+            onChange={changeValue}
+            type="text"
+            id="form-personalDetail-phone"
+          />
+          <label htmlFor="form-personalDetail-address">Address:</label>
+          <input
+            onChange={changeValue}
+            type="text"
+            id="form-personalDetail-address"
+          />
         </section>
         <section className="form-education form">
-          <div className="education-title">Education:</div>
-          <label htmlFor="education-InstitutionName">Institution Name:</label>
-          <input type="text" id="education-InstitutionName" />
-          <label htmlFor="education-degree">Degree:</label>
-          <input type="text" id="education-degree" />
-          <label htmlFor="education-startDate">Start Date:</label>
-          <input type="text" id="education-startDate" />
-          <label htmlFor="education-endDate">End Date:</label>
-          <input type="text" id="education-endDate" />
+          <div className="form-education-title">Education:</div>
+          <label htmlFor="form-education-institutionName">
+            Institution Name:
+          </label>
+          <input
+            onChange={changeValue}
+            type="text"
+            id="form-education-institutionName"
+          />
+          <label htmlFor="form-education-degree">Degree:</label>
+          <input
+            onChange={changeValue}
+            type="text"
+            id="form-education-degree"
+          />
+          <label htmlFor="form-education-startDate">Start Date:</label>
+          <input
+            onChange={changeValue}
+            type="text"
+            id="form-education-startDate"
+          />
+          <label htmlFor="form-education-endDate">End Date:</label>
+          <input
+            onChange={changeValue}
+            type="text"
+            id="form-education-endDate"
+          />
         </section>
 
         <section className="form-experience form">
-          <div className="experience-title">Experience:</div>
-          <label htmlFor="experience-company">Company:</label>
-          <input type="text" id="experience-company" />
-          <label htmlFor="experience-role">Role:</label>
-          <input type="text" id="experience-role" />
-          <label htmlFor="experience-startDate">Start Date:</label>
-          <input type="text" id="experience-startDate" />
-          <label htmlFor="experience-endDate">End Date:</label>
-          <input type="text" id="experience-endDate" />
-          <label htmlFor="experience-description">Role description:</label>
+          <div className="form-experience-title">Experience:</div>
+          <label htmlFor="form-experience-company">Company:</label>
+          <input
+            onChange={changeValue}
+            type="text"
+            id="form-experience-company"
+          />
+          <label htmlFor="form-experience-role">Role:</label>
+          <input onChange={changeValue} type="text" id="form-experience-role" />
+          <label htmlFor="form-experience-startDate">Start Date:</label>
+          <input
+            onChange={changeValue}
+            type="text"
+            id="form-experience-startDate"
+          />
+          <label htmlFor="form-experience-endDate">End Date:</label>
+          <input
+            onChange={changeValue}
+            type="text"
+            id="form-experience-endDate"
+          />
+          <label htmlFor="form-experience-description">Role description:</label>
           <textarea
-            name="experience-description"
-            id="experience-description"
+            onChange={changeValue}
+            name="form-experience-description"
+            id="form-experience-description"
             cols="30"
             rows="5"
           ></textarea>
         </section>
         <section className="form-personal form">
-          <div className="personal-title">Personal:</div>
-          <label htmlFor="personal-content"></label>
+          <div className="form-personal-title">Personal:</div>
+          <label htmlFor="form-personal-content"></label>
           <textarea
-            name="personal-content"
-            id="personal-content"
+            onChange={changeValue}
+            name="form-personal-content"
+            id="form-personal-content"
             cols="30"
             rows="5"
           ></textarea>
