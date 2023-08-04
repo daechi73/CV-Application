e744256aa1172b0be12468e3d83a2d3fbdba52c2
J <59581793+daechi73@users.noreply.github.com>
2023-08-04 18:52:55 -0400

    Header, content section with form and CV component added

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import "./App.css";
 import Header from "./component/Header";
 import Form from "./component/Form";
 import CV from "./component/CV";
+import CVInfoHolder from "./component/CV-infoHolder";
 
 function App() {
+  const cvInfoHolder = CVInfoHolder();
+  const [value, setValue] = useState(cvInfoHolder);
   return (
     <>
       <Header />
       <div className="content">
-        <Form />
-        <CV />
+        <Form setValue={setValue} inputValue={value} />
+        <CV inputValue={value} />
       </div>
     </>
   );
