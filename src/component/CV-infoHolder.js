82c0b6f7aab3e7a014f13a117a4ca71c9c80d173
J <59581793+daechi73@users.noreply.github.com>
2023-08-04 18:57:59 -0400

    returns an object literal that holds all the user input from the forms section

---
+++ src/component/CV-infoHolder.js
@@ -0,0 +1,20 @@
+const CVInfoHolder = () => {
+  return {
+    "CV-personalDetail-name": "",
+    "CV-personalDetail-email": "",
+    "CV-personalDetail-phone": "",
+    "CV-personalDetail-address": "",
+    "CV-education-institutionName": "",
+    "CV-education-degree": "",
+    "CV-education-startDate": "",
+    "CV-education-endDate": "",
+    "CV-experience-company": "",
+    "CV-experience-role": "",
+    "CV-experience-startDate": "",
+    "CV-experience-endDate": "",
+    "CV-experience-description": "",
+    "CV-experience-personal-content": "",
+  };
+};
+
+export default CVInfoHolder;
