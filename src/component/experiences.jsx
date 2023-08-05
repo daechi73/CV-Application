02f8ad997a8667d53aec4dafd59f61b90f28397d
J <59581793+daechi73@users.noreply.github.com>
2023-08-05 18:58:42 -0400

    allocated rendering of experiences section to its own component

---
+++ src/component/experiences.jsx
@@ -0,0 +1,21 @@
+const experiencesSection = () => {
+  return (
+    <section className="CV-experience">
+      <div id="CV-experience-company">
+        {props.inputValue["CV-experience-company"]}
+      </div>
+      <div id="CV-experience-role">
+        {props.inputValue["CV-experience-role"]}
+      </div>
+      <div id="CV-experience-startDate">
+        {props.inputValue["CV-experience-startDate"]}
+      </div>
+      <div id="CV-experience-endDate">
+        {props.inputValue["CV-experience-endDate"]}
+      </div>
+      <div id="CV-experience-description">
+        {props.inputValue["CV-experience-description"]}
+      </div>
+    </section>
+  );
+};
