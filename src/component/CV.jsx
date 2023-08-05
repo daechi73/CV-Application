b18415c59cfdea2334c164c81542e7bdc1e0a154
J <59581793+daechi73@users.noreply.github.com>
2023-08-05 19:25:22 -0400

    added section components

--- src/component/CV.jsx
+++ src/component/CV.jsx
@@ -1,12 +1,30 @@
 import "./CV.css";
-import PersonalDetail from "./PersonalDetail";
+import PersonalDetailSection from "./PersonalDetail";
+import ExperiencesSection from "./Experiences";
+import EducationSection from "./Education";
+import PersonalSection from "./PersonalDetail";
 
 const CV = (props) => {
   return (
     <>
       <div className="container-CV">
         {props.activateSections.personalDetail ? (
-          <PersonalDetail inputValue={props.inputValue} />
+          <PersonalDetailSection inputValue={props.inputValue} />
+        ) : (
+          ""
+        )}
+        {props.activateSections.education ? (
+          <EducationSection inputValue={props.inputValue} />
+        ) : (
+          ""
+        )}
+        {props.activateSections.experience ? (
+          <ExperiencesSection inputValue={props.inputValue} />
+        ) : (
+          ""
+        )}
+        {props.activateSections.personal ? (
+          <PersonalSection inputValue={props.inputValue} />
         ) : (
           ""
         )}
