68725542ae9ffaa451e022ff6d91d621117a1272
J <59581793+daechi73@users.noreply.github.com>
2023-08-05 19:09:42 -0400

    commited to see past commits

--- src/component/CV.jsx
+++ src/component/CV.jsx
@@ -1,5 +1,5 @@
 import "./CV.css";
-import PersonalDetail from "./personalDetail";
+import PersonalDetail from "./PersonalDetail";
 
 const CV = (props) => {
   return (
@@ -10,42 +10,6 @@ const CV = (props) => {
         ) : (
           ""
         )}
-        <section id="CV-education">
-          <div id="CV-education-institutionName">
-            {props.inputValue["CV-education-institutionName"]}
-          </div>
-          <div id="CV-education-degree">
-            {props.inputValue["CV-education-degree"]}
-          </div>
-          <div id="CV-education-startDate">
-            {props.inputValue["CV-education-startDate"]}
-          </div>
-          <div id="CV-education-endDate">
-            {props.inputValue["CV-education-endDate"]}
-          </div>
-        </section>
-        <section className="CV-experience">
-          <div id="CV-experience-company">
-            {props.inputValue["CV-experience-company"]}
-          </div>
-          <div id="CV-experience-role">
-            {props.inputValue["CV-experience-role"]}
-          </div>
-          <div id="CV-experience-startDate">
-            {props.inputValue["CV-experience-startDate"]}
-          </div>
-          <div id="CV-experience-endDate">
-            {props.inputValue["CV-experience-endDate"]}
-          </div>
-          <div id="CV-experience-description">
-            {props.inputValue["CV-experience-description"]}
-          </div>
-        </section>
-        <section className="CV-personal">
-          <div id="CV-personal-content">
-            {props.inputValue["CV-personal-content"]}
-          </div>
-        </section>
       </div>
     </>
   );
