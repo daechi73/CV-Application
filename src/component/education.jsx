b28dbdfb24e60770e21f404c4a3e3064b21a1664
J <59581793+daechi73@users.noreply.github.com>
2023-08-05 18:58:18 -0400

    allocated rendering of education section to its own component

---
+++ src/component/education.jsx
@@ -0,0 +1,19 @@
+const educationSection = () => {
+  return (
+    <section id="CV-education">
+      <div id="CV-education-institutionName">
+        {props.inputValue["CV-education-institutionName"]}
+      </div>
+      <div id="CV-education-degree">
+        {props.inputValue["CV-education-degree"]}
+      </div>
+      <div id="CV-education-startDate">
+        {props.inputValue["CV-education-startDate"]}
+      </div>
+      <div id="CV-education-endDate">
+        {props.inputValue["CV-education-endDate"]}
+      </div>
+    </section>
+  );
+};
+export default educationSection;
