6bafa3d88f23d842d7e4ac80be877ceaa34dd7de
J <59581793+daechi73@users.noreply.github.com>
2023-08-07 01:12:24 -0400

    allocated file dir

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Form from "./components/Form";
 import CV from "./components/CV";
-import { CVInfoHolder, CVSectionPass } from "./components/UseStateInfoHolders";
+import { CVInfoHolder, CVSectionPass } from "./logic/UseStateInfoHolders";
 
 function App() {
   const cvInfoHolder = CVInfoHolder();
