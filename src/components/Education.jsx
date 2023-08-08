81716e88469bfefd38dfa01e14e5d58f6b07c6ab
J <59581793+daechi73@users.noreply.github.com>
2023-08-08 00:15:36 -0400

    added labels for each output

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,16 +1,21 @@
 const EducationSection = (props) => {
   return (
     <section id="CV-education">
-      <div id="CV-education-institutionName">
+      <div className="CV-education-title">Education:</div>
+      <div id="CV-education-institutionName" className="CV-education">
+        <span className="label-education-institutionName"> School Name: </span>
         {props.inputValue["CV-education-institutionName"]}
       </div>
-      <div id="CV-education-degree">
+      <div id="CV-education-degree" className="CV-education">
+        <span className="label-education-degree">Degree: </span>
         {props.inputValue["CV-education-degree"]}
       </div>
-      <div id="CV-education-startDate">
+      <div id="CV-education-startDate" className="CV-education">
+        <span className="label-education-startDate">Start Date: </span>
         {props.inputValue["CV-education-startDate"]}
       </div>
-      <div id="CV-education-endDate">
+      <div id="CV-education-endDate" className="CV-education">
+        <span className="label-education-endDate">End Date: </span>
         {props.inputValue["CV-education-endDate"]}
       </div>
     </section>
