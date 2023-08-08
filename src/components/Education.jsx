6095580e3fb21f147df6316e2d0fa61e1d14ac87
J <59581793+daechi73@users.noreply.github.com>
2023-08-08 01:02:02 -0400

    divided CV.css to its own section and added basic styles

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,22 +1,26 @@
+import "../css/Education.css";
+
 const EducationSection = (props) => {
   return (
     <section id="CV-education">
       <div className="CV-education-title">Education:</div>
-      <div id="CV-education-institutionName" className="CV-education">
-        <span className="label-education-institutionName"> School Name: </span>
-        {props.inputValue["CV-education-institutionName"]}
-      </div>
-      <div id="CV-education-degree" className="CV-education">
-        <span className="label-education-degree">Degree: </span>
-        {props.inputValue["CV-education-degree"]}
-      </div>
-      <div id="CV-education-startDate" className="CV-education">
-        <span className="label-education-startDate">Start Date: </span>
-        {props.inputValue["CV-education-startDate"]}
-      </div>
-      <div id="CV-education-endDate" className="CV-education">
-        <span className="label-education-endDate">End Date: </span>
-        {props.inputValue["CV-education-endDate"]}
+      <div className="CV-education-content">
+        <div id="CV-education-institutionName" className="CV-education">
+          <span className="label-education-institutionName">School Name:</span>
+          {props.inputValue["CV-education-institutionName"]}
+        </div>
+        <div id="CV-education-degree" className="CV-education">
+          <span className="label-education-degree">Degree: </span>
+          {props.inputValue["CV-education-degree"]}
+        </div>
+        <div id="CV-education-startDate" className="CV-education">
+          <span className="label-education-startDate">Start Date: </span>
+          {props.inputValue["CV-education-startDate"]}
+        </div>
+        <div id="CV-education-endDate" className="CV-education">
+          <span className="label-education-endDate">End Date: </span>
+          {props.inputValue["CV-education-endDate"]}
+        </div>
       </div>
     </section>
   );
--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,26 +1,30 @@
+import "../css/Experiences.css";
+
 const ExperiencesSection = (props) => {
   return (
     <section className="CV-experience">
       <div className="CV-experience-title">Experience:</div>
-      <div id="CV-experience-company">
-        <span className="label-experience-company">Company: </span>
-        {props.inputValue["CV-experience-company"]}
-      </div>
-      <div id="CV-experience-role">
-        <span className="label-experience-role">Role: </span>
-        {props.inputValue["CV-experience-role"]}
-      </div>
-      <div id="CV-experience-startDate">
-        <span className="label-experience-startDate">Start Date: </span>
-        {props.inputValue["CV-experience-startDate"]}
-      </div>
-      <div id="CV-experience-endDate">
-        <span className="label-experience-endDate">End Date: </span>
-        {props.inputValue["CV-experience-endDate"]}
-      </div>
-      <div id="CV-experience-description">
-        <span className="label-experience-description">Description: </span>
-        {props.inputValue["CV-experience-description"]}
+      <div className="CV-experience-content">
+        <div id="CV-experience-company">
+          <span className="label-experience-company">Company: </span>
+          {props.inputValue["CV-experience-company"]}
+        </div>
+        <div id="CV-experience-role">
+          <span className="label-experience-role">Role: </span>
+          {props.inputValue["CV-experience-role"]}
+        </div>
+        <div id="CV-experience-startDate">
+          <span className="label-experience-startDate">Start Date: </span>
+          {props.inputValue["CV-experience-startDate"]}
+        </div>
+        <div id="CV-experience-endDate">
+          <span className="label-experience-endDate">End Date: </span>
+          {props.inputValue["CV-experience-endDate"]}
+        </div>
+        <div id="CV-experience-description">
+          <span className="label-experience-description">Description: </span>
+          {props.inputValue["CV-experience-description"]}
+        </div>
       </div>
     </section>
   );
